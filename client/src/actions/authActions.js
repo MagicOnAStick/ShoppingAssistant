24d7fbecf9732f963b3e6f08a0452f8c1240242c
Julian <magic.onastick@gmail.com>
2019-01-03 16:01:14 +0100

    login functionality

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,7 +1,9 @@
-import { GET_ERRORS } from './types';
+import { GET_ERRORS, SET_CURRENT_USER } from './types';
 
 //axios is an http client (alternative to js fetch api)
 import axios from 'axios';
+import setAuthToken from '../utils/setAuthToken';
+import jwt_decode from 'jwt-decode';
 
 export const registerUser = (userData, history) => dispatch => {
     axios.post('/api/users/register',userData)
@@ -14,4 +16,35 @@ export const registerUser = (userData, history) => dispatch => {
     );
 };
 
-//inside component redirect works as follows: this.props.history.push('/landing');
+export const loginUser = userData => dispatch => {
+    axios.post('/api/users/login', userData)
+    .then(res => {
+        //save to localstorage
+        const { token } = res.data;
+
+        //set to localstorage
+        localStorage.setItem('jwtToken', token);
+
+        //set token to authheader
+        setAuthToken(token);
+
+        //decode tokenstring to get user data
+        const decoded = jwt_decode(token);
+
+        //set current user
+        dispatch(setCurrentUser(decoded));
+    })
+    .catch(err => 
+        dispatch({
+            type: GET_ERRORS,
+            payload: err.response.data
+        })
+    );
+};
+
+export const setCurrentUser = decoded =>{
+    return {
+        type: SET_CURRENT_USER,
+        payload: decoded
+    }
+}
--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -4,3 +4,4 @@ export const DELETE_ITEM = 'DELETE_ITEM';
 export const ITEMS_LOADING = 'ITEMS_LOADING';
 
 export const GET_ERRORS = 'GET_ERRORS';
+export const SET_CURRENT_USER = 'SET_CURRENT_USER';
