d830c217efb134dde9a6fe6a5450cab2de49e861
Julian <info@aw-medienagentur.de>
2019-06-17 20:41:21 +0200

    dashboard routing

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -1,34 +1,42 @@
-import axios from 'axios';
-import { GET_PROFILE, PROFILE_LOADING, GET_ERRORS, CLEAR_CURRENT_PROFILE } from './types';
+import axios from "axios";
+import { setAlert } from "./alert";
+import {
+	GET_PROFILE,
+	PROFILE_LOADING,
+	PROFILE_ERROR,
+	CLEAR_CURRENT_PROFILE
+} from "./types";
 
 // Get current profile
-export const getCurrentProfile = () => dispatch =>{
-    dispatch(setProfileLoading());
-    axios.get('/api/profile')
-        .then(res =>
-            dispatch({
-                type: GET_PROFILE,
-                payload: res.data
-            })
-        )
-        .catch(err =>
-            dispatch({
-                //return empty profile
-                type: GET_PROFILE,
-                payload: {}
-            }));
-}
+export const getCurrentProfile = () => async dispatch => {
+	dispatch(setProfileLoading());
+	await axios
+		.get("/api/profile")
+		.then(res =>
+			dispatch({
+				type: GET_PROFILE,
+				payload: res.data
+			})
+		)
+		.catch(err =>
+			dispatch({
+				//return empty profile
+				type: GET_PROFILE,
+				payload: { msg: err.response.statusText, status: err.respnse.status }
+			})
+		);
+};
 
 // Profile Loading
 //just informs the reducer that the profile is loading - no payload / data required
 export const setProfileLoading = () => {
-    return{
-        type: PROFILE_LOADING
-    }
-}
+	return {
+		type: PROFILE_LOADING
+	};
+};
 
 export const clearCurrentProfile = () => {
-    return{
-        type: CLEAR_CURRENT_PROFILE
-    }
-}
+	return {
+		type: CLEAR_CURRENT_PROFILE
+	};
+};
--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,13 +1,15 @@
-export const GET_ITEMS = 'GET_ITEMS';
-export const ADD_ITEM = 'ADD_ITEM';
-export const DELETE_ITEM = 'DELETE_ITEM';
-export const ITEMS_LOADING = 'ITEMS_LOADING';
+export const GET_ITEMS = "GET_ITEMS";
+export const ADD_ITEM = "ADD_ITEM";
+export const DELETE_ITEM = "DELETE_ITEM";
+export const ITEMS_LOADING = "ITEMS_LOADING";
 
-export const GET_ERRORS = 'GET_ERRORS';
-export const SET_CURRENT_USER = 'SET_CURRENT_USER';
+export const GET_ERRORS = "GET_ERRORS";
+export const SET_CURRENT_USER = "SET_CURRENT_USER";
 
-export const GET_PROFILE = 'GET_PROFILE';
-export const PROFILE_LOADING = 'PROFILE_LOADING';
-export const PROFILE_NOT_FOUND = 'PROFILE_NOT FOUND';
-export const CLEAR_CURRENT_PROFILE = 'CLEAR_CURRENT_PROFILE';
-export const GET_PROFILES = 'GET_PROFILES';
+export const GET_PROFILE = "GET_PROFILE";
+export const PROFILE_ERROR = "PROFILE_ERROR";
+
+export const PROFILE_LOADING = "PROFILE_LOADING";
+export const PROFILE_NOT_FOUND = "PROFILE_NOT FOUND";
+export const CLEAR_CURRENT_PROFILE = "CLEAR_CURRENT_PROFILE";
+export const GET_PROFILES = "GET_PROFILES";
