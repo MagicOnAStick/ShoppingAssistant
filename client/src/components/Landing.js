cc487d3553b647ffe4c3278fa3e5ea3e52f40346
Julian <info@aw-medienagentur.de>
2019-06-18 07:58:29 +0200

    spinner component

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,34 +1,46 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { Link, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
 
-class Landing extends Component {
-  render() {
-    //TODO render other nav buttons if logged inj
-    return (
-      <div className="landing">
-        <div className="dark-overlay landing-inner text-light">
-          <div className="container">
-            <div className="row">
-              <div className="col-md-12 text-center">
-                <h1 className="display-4 mb-4">ShoppingList</h1>
-                <p className="lead">
-                  {' '}
-                  Create ShoppingLists easily on any device and use them comfy while shopping without any paperlists.
-                </p>
-                <hr />
-                <Link to="/register" className="btn btn-lg btn-info mr-2">
-                  Sign Up
-                </Link>
-                <Link to="/login" className="btn btn-lg btn-light">
-                  Login
-                </Link>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
-}
+const Landing = ({ isAuthenticated }) => {
+	if (isAuthenticated) {
+		return <Redirect to="/dashboard" />;
+	}
 
-export default Landing;
+	return (
+		<div className="landing">
+			<div className="dark-overlay landing-inner text-light">
+				<div className="container">
+					<div className="row">
+						<div className="col-md-12 text-center">
+							<h1 className="display-4 mb-4">ShoppingList</h1>
+							<p className="lead">
+								{" "}
+								Create ShoppingLists easily on any device and use them comfy
+								while shopping without any paperlists.
+							</p>
+							<hr />
+							<Link to="/register" className="btn btn-lg btn-info mr-2">
+								Sign Up
+							</Link>
+							<Link to="/login" className="btn btn-lg btn-light">
+								Login
+							</Link>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+Landing.propTypes = {
+	isAuthenticated: PropTypes.bool
+};
+
+const mapStateToProps = state => ({
+	isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect(mapStateToProps)(Landing);
